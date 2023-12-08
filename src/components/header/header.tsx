68f8ff309617161f65ab2d16492e0377a69598bc
RC-3222 <tomgar9@outlook.com>
2023-12-08 16:27:28 +0300

    update

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -8,16 +8,14 @@ export const Header: FC = () => {
   const [isModalOpen, setIsModalOpen] = useState(false)
 
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar color='primary' position='static'>
-        <Typography variant='h6' color='inherit' component='div'>
-          <header className={styles.header}>
-            <span className={styles.header__name}>Note Manager</span>
-            <Button color='secondary' onClick={() => setIsModalOpen(true)} variant='outlined'>Add note</Button>
-          </header>
-          <AddNoteModal isOpen={isModalOpen} handleClose={() => setIsModalOpen(false)} />
-        </Typography>
-      </AppBar>
-    </Box >
+    <AppBar color='primary' position='static'>
+      <Typography variant='h6' color='inherit' component='div'>
+        <header className={styles.header}>
+          <span className={styles.header__name}>Note Manager</span>
+          <Button color='secondary' onClick={() => setIsModalOpen(true)} variant='outlined'>Add note</Button>
+        </header>
+        <AddNoteModal isOpen={isModalOpen} handleClose={() => setIsModalOpen(false)} />
+      </Typography>
+    </AppBar>
   );
 };
